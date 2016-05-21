2f53b7661fd23c0703d1334c32fa30160736cab0
Sheonna Harris <svonharris@gmail.com>
2016-05-21 10:48:14 -0400

    Removed old/unused files. Added functionality to verify an input's name attr and execute an operation accordingly.

--- scripts/function.js
+++ scripts/function.js
@@ -31,17 +31,20 @@ $(document).ready(function() {
         //console.log("field filled out");
         correctClass(input);
       }
-    } else {
-      console.log("NR");
     }
   }
 
   function errorClass(element) {
     $(element).parent().addClass('error');
+    $('#js-submit').addClass('disabled');
   }
 
   function correctClass(element) {
     $(element).parent().removeClass('error');
+    var n = $('.js-input').hasClass('error');
+    if (!n) {
+      $('#js-submit').removeClass('disabled');
+    }
   }
 
 
@@ -49,7 +52,12 @@ $(document).ready(function() {
 
   $('#js-submit').click(function(e) {
     e.preventDefault();
-    alert("Thanks");
+    $(this).addClass('active');
+    $(this).blur(function() {
+      $(this).removeClass('active');
+    });
+    signUpForm($('#signup'));
+    // alert("Thanks");
     // e.preventDefault(msg, function() {
     //   if (msg === success) {
     //     alert("Thanks");
@@ -59,17 +67,192 @@ $(document).ready(function() {
     //   }
     // });
   });
-  $('#js-submit').keypress(function(e) {
-    if (event.which == 13) {
-      $(this).addClass('active');
-      $(this).blur(function() {
-        $(this).removeClass('active');
-      });
+
+  function signUpForm(form) {
+    var inputs = [];
+    $(form).find('input').each(function() {
+      inputs.push($(this));
+    }); 
+    //console.log(inputs.length); 
+    verify(inputs);
+  }
+
+  function verify(inputsArr) {
+    for (var i=0; i<inputsArr.length; i++) {
+      var n = inputsArr[i].attr('name');
+      var v = inputsArr[i].val();    
+      switch(n) {
+        case "phone":
+          console.log(v + " is a phone number");
+          phoneNumber(v);
+          break;
+        
+        case "fname":
+          console.log(v + " is a first name");
+          break;
+        
+        case "lname":
+          console.log(v + " is a last name");
+          break;
+        
+        case "email":
+          console.log(v + " is an email");
+          break;
+        
+        case "birthdate":
+          console.log(v + " is a birthdate");
+          break; 
+        
+        default:
+          console.log(v + " is undefined");
+          return;
+      }
     }
+  }
+
+  function phoneNumber(phone) {
+    if (/\D/g.test(phone)) {
+      console.log("incorrect entry");
+      //errorClass(phone);
+    } else if (phone.length !== 10) {
+      console.log("must have 10 digits");
+    }
+  }
+
+
+
+
+
+
+/*
+<form action="#" method="post" id="myform" name="myform">
+
+  
+  <label for="fullname" class="item">Full Name*</label>
+  <input class="put" type="text" id="fullname" name="name" autocomplete="name" required/>
+  <div class="none">error</div><br><br>
+
+  
+  <label for="email" class="item">Email Address*</label>
+  <input class="put" type="email" id="email" name="email" autocomplete="email" required/><br><br>
+
+  
+  <label for="phone" class="item">Phone Number</label>
+  <input class="put" type="tel" id="phone" name="phone" autocomplete="tel" pattern="[0-9]" maxlength="10" size="10" /><br><br>
+
+  
+  <div class="formsubmit">
+    <button type="submit" class="disabled btn" id="submit">submit</button>
+    <button type="reset">reset</button>
+  </div>
+  
+</form>
+*/
+
+
+/*
+$(document).ready(function() {
+    highlight();
+  
+});
+
+
+function highlight() {
+  $(':input').focus(function() {
+    var input = $(this);
+    input.addClass('focused');
+    input.blur(function() {
+      input.removeClass('focused');
+      required(input);
+    });
   });
+}   
+
+function required(input) {
+  if (input.prop('required')) {
+    var v = input.val();
+    if (v === null || v === "") {
+      //console.log("field empty");
+      errorClass(input);
+    } else {      
+      correctClass(input);
+      //field is not empty but does it make sense??
+    }
+  }
+}
 
 
-		
+
+
+function errorClass(element) {
+  $(element).addClass('error');
+  $('#submit').addClass('disabled');
+}
+
+function correctClass(element) {
+ $(element).removeClass('error');
+  var n = $('input').hasClass('error');
+  if (!n) {
+    $('#submit').removeClass('disabled');
+  }
+
+}
 
 
 
+
+
+$('#submit').click(function(e) {
+  e.preventDefault();
+  submitForm($('#myform'));
+});
+
+function submitForm(form) {
+  var inputs = [];
+  $(form).find('input').each(function() {
+    inputs.push($(this));
+  }); 
+  //console.log(inputs.length); 
+  verify(inputs);
+}
+
+function verify(inputsArr) {
+  for (var i=0; i<inputsArr.length; i++) {
+    var n = inputsArr[i].attr('name');
+    var v = inputsArr[i].val();    
+    switch(n) {
+      case "phone":
+        console.log(v + " is a phone number");
+        //phoneNumber(v);
+        phoneNumber(inputsArr[i]);
+        break;
+      case "name":
+        console.log(v + " is a name");
+        break;
+      case "email":
+        console.log(v + " is an email");
+        break;
+      case "birthdate":
+        console.log(v + " is a birthdate");
+        break; 
+      default:
+        console.log(v + " is undefined");
+        return;
+    }
+  }
+  var k = $('input').hasClass('error');
+  if (!k) {
+    alert("submit form");
+  }
+}
+
+function phoneNumber(phoneVal) {
+  if (/\D/g.test(phoneVal.val())) {
+    console.log("incorrect entry");
+    errorClass(phoneVal);
+  } else if (phoneVal.length !== 10) {
+    console.log("must have 10 digits");
+  }
+}
+
+*/

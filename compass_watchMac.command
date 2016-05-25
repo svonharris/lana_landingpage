0435bb93db6e9cfe17946b95299503125b2943b5
Sheonna Harris <svonharris@gmail.com>
2016-05-24 20:46:28 -0400

    refactered form validation functions and setup object for server submission.

--- compass_watchMac.command
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#cd desktop/javascript_projects/lana_landingpage;
-compass watch css/sass/all.scss
--- scripts/function.js
+++ scripts/function.js
@@ -1,3 +1,8 @@
+/*
+Quirks
+   - won't allow one letter to pass as a name(bad?)
+   - current name accepts all symbols
+*/
 
 $(document).ready(function() {
     fade();
@@ -26,10 +31,12 @@ $(document).ready(function() {
       var n = input.val();
       if (n === null || n === "") {
         errorClass(input);
-        disable()
+        disable();
       } else {
         correctClass(input);
       }
+    } else {
+      correctClass(input);
     }
   }
 
@@ -54,18 +61,17 @@ $(document).ready(function() {
 
   $('#js-submit').click(function(e) {
     e.preventDefault();
-    var btn = $(this);
-    buttonActive(btn);    
-    signUpForm($('#signup'));
-    // alert("Thanks");
-    // e.preventDefault(msg, function() {
-    //   if (msg === success) {
-    //     alert("Thanks");
-    //     $('.button--submit').html("Good Luck!");
-    //   } else {
-    //     alert("submit failed");
-    //   }
-    // });
+    buttonActive($(this));    
+    var signUpForm = verification($('#signup'));
+    // console.log(signUpForm);
+    $.ajax({
+      type: 'post',
+      url: 'server.rb',
+      data: 'signUpForm',
+      dataType: 'json'
+    }).done(function(msg) {
+      // console.log(msg);
+    });
   });
 
   function buttonActive(btn) {
@@ -75,100 +81,63 @@ $(document).ready(function() {
     });
   }
 
-  function signUpForm(form) {
+
+
+  function verification(form) {
     var inputs = [];
     $(form).find('input').each(function() {
       inputs.push($(this));
-    }); 
-    //console.log(inputs.length); 
-    verify(inputs);
-  }
+    });
 
-  function verify(inputsArr) {
-    for (var i=0; i<inputsArr.length; i++) {
-      var n = inputsArr[i].attr('name');
-      var v = inputsArr[i].val();    
+    var customer = {};
+    for (var i=0; i<inputs.length; i++) {
+      var n = inputs[i].attr('name');
+      var v = inputs[i].val();
+      var correctInputs = [];
       switch(n) {
         case "phone":
           var pn = phoneNumber(v);
-          if (pn === false) {
-            errorClass(inputsArr[i]);
-            console.log("phone number must have 10 digits");
-          }
+          shit(pn,inputs[i],"Must have 10 digits",n,v);    
           break;
         
         case "fname":
           var bn = birthName(v);
-          if (bn === false) {
-            errorClass(inputsArr[i]);
-            console.log("Name must be alphabetic characters only");
-          }
+          shit(bn,inputs[i],"Alphabetical characters only",n,v); 
           break;
         
         case "lname":
           var bn = birthName(v);
-          if (bn === false) {
-            errorClass(inputsArr[i]);
-            console.log("Name must be alphabetic characters only");
-          }
+          shit(bn,inputs[i],"Alphabetical characters only",n,v);
           break;
         
         case "email":
           var eaddy = emailAddres(v);
-          if (eaddy === false) {
-            errorClass(inputsArr[i]);
-            console.log("email is incorrect");
-          }
+          shit(eaddy,inputs[i],"Email is incorrect",n,v);
           break;
         
         case "birthdate":
           var bday = birthDate(v);
-          if (bday === false) {
-            errorClass(inputsArr[i]);
-            console.log("Birthdate must be MM/DD/YYYY");
-          }
+          shit(bday,inputs[i],"MM/DD/YYYY format",n,v);
           break; 
-        
+
         default:
           console.log(v + " is undefined");
           return;
       }
     }
-  }
 
-  function phoneNumber(phone) {
-    if (phone === null || phone === "") {
-      return true;
-    } else {
-      var regex = /^\d{10}$/;
-      if (!regex.test(phone)) {
-        return false;
-      }
-    }
-  }
-  function emailAddres(email) {
-    var regex = /^[\w\.\-\_\+]+@[\w-]+\.\w{2,4}$/;
-    if (!regex.test(email)) {
-      return false;
-    }
-  }
-  function birthName(name) {
-    var regex = /^[a-zA-Z]+\w[\w\-\ ]+[a-zA-Z]+$/;
-    if (regex.test(name)) {
-      var regex = /[0-9]/;
-      if (regex.test(name)) {
-        return false;
+    function shit(patternval,inputnum,text,name,value) {
+      if (patternval === false) {
+        correctInputs.push("false");
+        errorMsg(inputnum, text);
       } else {
-        return true;
+        customer[name] = value;
+        successMsg(inputnum, "");
       }
-    } else {
-      return false;
     }
-  }
-  function birthDate(bday) {
-    var regex = /^\d{2}\/\d{2}\/\d{4}$/;
-    if (!regex.test(bday)) {
-      return false;
+
+    if (correctInputs.indexOf('false') === -1) {
+      return customer;
     }
   }
 
@@ -176,135 +145,52 @@ $(document).ready(function() {
 
 
 
-/*
-<form action="#" method="post" id="myform" name="myform">
 
-  
-  <label for="fullname" class="item">Full Name*</label>
-  <input class="put" type="text" id="fullname" name="name" autocomplete="name" required/>
-  <div class="none">error</div><br><br>
-
-  
-  <label for="email" class="item">Email Address*</label>
-  <input class="put" type="email" id="email" name="email" autocomplete="email" required/><br><br>
-
-  
-  <label for="phone" class="item">Phone Number</label>
-  <input class="put" type="tel" id="phone" name="phone" autocomplete="tel" pattern="[0-9]" maxlength="10" size="10" /><br><br>
-
-  
-  <div class="formsubmit">
-    <button type="submit" class="disabled btn" id="submit">submit</button>
-    <button type="reset">reset</button>
-  </div>
-  
-</form>
-*/
-
-
-/*
-$(document).ready(function() {
-    highlight();
-  
-});
-
-
-function highlight() {
-  $(':input').focus(function() {
-    var input = $(this);
-    input.addClass('focused');
-    input.blur(function() {
-      input.removeClass('focused');
-      required(input);
-    });
-  });
-}   
-
-function required(input) {
-  if (input.prop('required')) {
-    var v = input.val();
-    if (v === null || v === "") {
-      //console.log("field empty");
-      errorClass(input);
-    } else {      
-      correctClass(input);
-      //field is not empty but does it make sense??
+    function errorMsg(field, msg) {
+      field.parent().next('.js-errormsg').html(msg).css('display', 'block');
+      errorClass(field);
     }
-  }
-}
-
-
-
-
-function errorClass(element) {
-  $(element).addClass('error');
-  $('#submit').addClass('disabled');
-}
-
-function correctClass(element) {
- $(element).removeClass('error');
-  var n = $('input').hasClass('error');
-  if (!n) {
-    $('#submit').removeClass('disabled');
-  }
-
-}
-
-
-
-
-
-$('#submit').click(function(e) {
-  e.preventDefault();
-  submitForm($('#myform'));
-});
-
-function submitForm(form) {
-  var inputs = [];
-  $(form).find('input').each(function() {
-    inputs.push($(this));
-  }); 
-  //console.log(inputs.length); 
-  verify(inputs);
-}
-
-function verify(inputsArr) {
-  for (var i=0; i<inputsArr.length; i++) {
-    var n = inputsArr[i].attr('name');
-    var v = inputsArr[i].val();    
-    switch(n) {
-      case "phone":
-        console.log(v + " is a phone number");
-        //phoneNumber(v);
-        phoneNumber(inputsArr[i]);
-        break;
-      case "name":
-        console.log(v + " is a name");
-        break;
-      case "email":
-        console.log(v + " is an email");
-        break;
-      case "birthdate":
-        console.log(v + " is a birthdate");
-        break; 
-      default:
-        console.log(v + " is undefined");
-        return;
+    function successMsg(field, msg) {
+      field.parent().next('.js-errormsg').html(msg).css('display', 'none');
+      correctClass(field);
     }
-  }
-  var k = $('input').hasClass('error');
-  if (!k) {
-    alert("submit form");
-  }
-}
+    function phoneNumber(phone) {
+      if (phone === null || phone === "") {
+        return true;
+      } else {
+        var regex = /^\d{10}$/;
+        if (!regex.test(phone)) {
+          return false;
+        }
+      }
+    }
+    function emailAddres(email) {
+      var regex = /^[\w\.\-\_\+]+@[\w-]+\.\w{2,4}$/;
+      if (!regex.test(email)) {
+        return false;
+      }
+    }
+    function birthName(name) {
+      // var regex = /^([a-zA-Z]+\w[\w\-\ ]+[a-zA-Z]$){0,}/;
+      // var regex = /((^[a-zA-Z])+[\w\-\ ]+([a-zA-Z]$)){0,}/;
+      var regex = /(^([a-zA-Z]*))+\w[\w\-\ ]/;
+      if (regex.test(name)) {
+        var regex = /[0-9]/;
+        if (regex.test(name)) {
+          return false;
+        } else {
+          return true;
+        }
+      } else {
+        return false;
+      }
+    }
+    function birthDate(bday) {
+      var regex = /^\d{2}\/\d{2}\/\d{4}$/;
+      if (!regex.test(bday)) {
+        return false;
+      }
+    }
 
-function phoneNumber(phoneVal) {
-  if (/\D/g.test(phoneVal.val())) {
-    console.log("incorrect entry");
-    errorClass(phoneVal);
-  } else if (phoneVal.length !== 10) {
-    console.log("must have 10 digits");
-  }
-}
 
-*/
+
